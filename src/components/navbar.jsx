403e38b40e77bd4bd15294dfd0a04e5c3bda7600
shubham sharma <shubhamtech004@gmail.com>
2025-06-28 03:31:49 +0530

    navbar dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,21 @@
 import { Database } from "lucide-react";
 import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function NavBar() {
   const auth = true;
+    const tempUser = {
+    name: 'Sam',
+    username: 'sam'
+  }
   return (
     <nav className="w-full flex justify-between px-8 h-12 items-center">
       <Link href="/" className="flex items-center gap-2">
@@ -12,7 +25,7 @@ export default function NavBar() {
         </span>
       </Link>
       {auth ? (
-        <p>User</p>
+        <UserModalComponent user={tempUser}/>
       ) : (
         <Link href="/sign-in" className="text-black dark:text-white">
           Sign In
@@ -20,4 +33,24 @@ export default function NavBar() {
       )}
     </nav>
   );
+}
+
+const UserModalComponent = ({ user }) => {
+    return <DropdownMenu className="bg-gray-100">
+            <DropdownMenuTrigger className="outline-none">
+              User
+                {/* <Image className="rounded-full border-2 border-[greenyellow]" src={user.image} width={30} height={30} /> */}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+            <DropdownMenuLabel>Hi,{user.name} </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+             <DropdownMenuItem> <Link href={`/profile/${user.username}`}>
+                    Go to Profile
+                </Link></DropdownMenuItem>
+            <DropdownMenuItem>
+              out
+            </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+  
 }
