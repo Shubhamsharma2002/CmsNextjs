35b07f7601f9fb6df1df6e94674005ceb2aff4c3
shubham sharma <shubhamtech004@gmail.com>
2025-06-30 00:00:13 +0530

    prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client"
-
+// The code you wrote is used to safely initialize and reuse a Prisma Client instance in both development and production environments.
 let prisma;
 
 if(process.env.NODE_ENV === 'production'){
