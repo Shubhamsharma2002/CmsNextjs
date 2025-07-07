4026bfa0ec33f776c106ef1f4f9edb54445a3922
shubham sharma <shubhamtech004@gmail.com>
2025-07-08 03:42:40 +0530

    save post api working and also save dthe data in db

--- src/app/api/(blog)/create/route.js
+++ src/app/api/(blog)/create/route.js
@@ -55,14 +55,66 @@ import { prisma } from "@/lib/prisma";
 //   }
 // }
 
-
-export async function GET(){
-    return NextResponse.json({message:"yes working"})
+export async function GET() {
+  return NextResponse.json({ message: "yes working" });
 }
 
-export async function POST(request){
-   const body = await request.json();
-   const {title,excerpt,category,ogImage,keywords,metaDescription,status ,slug,content}  = body;
-   return NextResponse.json({data:body});
-
+export async function POST(request) {
+  const session = await getAuthsession();
+  console.log("Session:", session);
+  if (!session || !session.user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  const body = await request.json();
+  const {
+    title,
+    excerpt,
+    category,
+    ogImage,
+    keywords,
+    metaDescription,
+    status,
+    slug,
+    content,
+  } = body;
+  if (!title || !content || !slug || !category || !session.user.id) {
+    return NextResponse.json({ message: "Missing fields" }, { status: 400 });
+  }
+  const statusOfPost = status || "DRAFT";
+ try {
+        let categoryCheck = await prisma.category.findUnique({
+            where: { slug: category }
+        })
+    console.log('categoryCheck first:', categoryCheck);
+        if(!categoryCheck){
+            categoryCheck = await prisma.category.create({
+                data: {
+                    title: category.charAt(0).toUpperCase() + category.slice(1),
+                    slug: category
+                }
+            })
+            
+        }
+    console.log('categoryCheck second:', categoryCheck);
+        const post = await prisma.post.create({
+            data: {
+                title,
+                content,
+                slug,
+                thumbnail: ogImage || null,
+                desc: metaDescription || null,
+                keywords: keywords || null,
+                excerpt: excerpt || null,
+                authorId: session.user.id,
+                catSlug: categoryCheck.slug,
+                status: statusOfPost
+            }
+        })
+        return NextResponse.json(post, { status: 201 })
+    } catch (error) {
+        console.log("error",error);
+        
+        console.log(error.message);
+        return NextResponse.json({ message: "Could not save post"}, { status: 500 })
+    }
 }
--- src/app/draft/page.jsx
+++ src/app/draft/page.jsx
@@ -1,8 +1,9 @@
 "use client"
 import Editor from "@/components/editor";
+import { NextResponse } from "next/server";
 
 export default function Draftblog() {
-   const savePost = ({title,excerpt,category,ogImage,keywords,metaDescription,status ,slug,content})=>{
+   const savePost = async({title,excerpt,category,ogImage,keywords,metaDescription,status ,slug,content})=>{
       //  console.log("title",title);
       //  console.log("excerpt",excerpt);
       //  console.log("cate",category);
@@ -12,7 +13,20 @@ export default function Draftblog() {
       //  console.log("status",status);
       //  console.log("content",content);
       //  console.log("slug",slug);
+       const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/create`,{
+             method:"POST",
+             headers:{
+              'Content-Type':'application/json',
+             }
+             ,body:JSON.stringify({title,excerpt,category,ogImage,keywords,metaDescription,status ,slug,content})
+       })
+       if(!res.ok){
+        // console.log(res);
+        
+        throw new Error("post not saved")
+       }
 
+        console.log(res.json());
         
    }
   return (
