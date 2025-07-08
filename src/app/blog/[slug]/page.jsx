9b4a832de2620a24311c10098ecb1f3505076c4d
shubham sharma <shubhamtech004@gmail.com>
2025-07-09 00:19:04 +0530

    read more option done with api calling using slug

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,29 +1,53 @@
 import dateFormat from "@/utils/dateFormate";
 import { Calendar } from "lucide-react";
 import Image from "next/image";
-const tempTags = "spacex, Nasa , Expolartion"
-export default function SingleBlog(){
-    return(
+// import "@/styles/blog.css"
+import { notFound } from "next/navigation";
+import Link from "next/link";
+const fetchSingleBlog = async(slug)=> {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get/${slug}`, { next: {tags: [slug]}})
+    if(res.status == 404){
+        notFound();
+    }
+    const data = await res.json();
+    console.log(data, 'single blog')
+    return data;
+}
+
+
+export default async function SingleBlog({params}){
+   const { slug } = params;
+    const post = await fetchSingleBlog(slug);
+
+    return (
         <section>
-           <div className="flex flex-col gap-4 items-center">
-             <Image src = "/thumbnails/react-v-next.png" width={500} height={250} alt="image" className="rounded border  w-[90%] md:w-[700px]" />
-             <div className="meta-of-a-blog space-y-2">
-                <div className="flex gap-2 items-center">
-                   <Calendar className="text-gray-400 size-4" />
-                   <p className="text-gray-400 text-xs">Created on: {dateFormat(new Date())}</p>
+            <div className="flex flex-col gap-4 items-center">
+                {post.thumbnail && <Image className="rounded border w-[90%] md:w-[700px]" src={post.thumbnail} width={500} height={250} alt={post.title}/>}
+                <h1 className="text-2xl md:text-4xl font-bold">{post.title}</h1>
+                <div className="meta-of-a-blog space-y-2">
+                    <div className="flex flex-col sm:flex-row gap-3">
+                        <div className="flex gap-2 items-center">
+                            <Calendar  className="text-gray-400 size-4" />
+                            <p className="text-gray-400 text-xs">Created on: {dateFormat(post.createdAt)}</p>
+                        </div>
+                        <Link className="flex items-center gap-2" href={`/user/${post.authorId}`}>
+                            <Image className="rounded-full" src={post.author.image} width={20} height={20}  alt="blog image"/>
+                            <p className="text-xs text-gray-400">{post.author.name}</p>
+                        </Link>
+                    </div>
+                    <div className="text-xs flex items-center gap-2">
+                        <p>Category:</p>
+                        <p className="badge bg-gray-600/30 border border-gray-700 w-fit px-2 py-1 rounded">{post.catSlug}</p>
+                    </div>
+                    {post?.keywords && <div className="text-xs flex items-center gap-2">
+                        <p>Tags:</p>
+                        {post?.keywords.split(",").map((tag, index)=> <p key={`${tag}_${index}`} className="badge bg-gray-600/30 border border-gray-700 w-fit px-[4px] py-[2px] rounded">{tag}</p>)}
+                    </div>}
                 </div>
-                <div className="text-xs flex items-center gap-2">
-                    <p>Category:</p>
-                     <p className="badge bg-gray-600/30 border border-gray-700 w-fit px-2 py-1 rounded">space expolatrion</p>
+                <div className="blogContent text-sm w-[90%] md:w-2/3 text-gray-300" dangerouslySetInnerHTML={{__html: post.content}}>
                 </div>
-                <div className="text-xs flex items-center gap-2">
-                     <p>Tags:</p>
-                     {tempTags.split(",").map((tag, index)=> <p key={`${tag}_${index}`} className="badge bg-gray-600/30 border border-gray-700 w-fit px-[4px] py-[2px] rounded">{tag}</p>)}
-                </div>
-                
-             </div>
-             <p className=" text-sm w-[90%] md:w-2/3 text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui non nisi accusamus modi adipisci voluptatibus ea maiores rem ab doloremque incidunt, amet voluptates, itaque suscipit laudantium optio atque aliquid enim consequatur expedita? Eaque, dignissimos enim sit odit ad, porro dolore voluptas nesciunt sunt culpa laudantium reiciendis, atque magnam perspiciatis exercitationem adipisci? Voluptatem quos repellat, rem dolorem temporibus eveniet obcaecati corrupti iusto, labore, voluptatum numquam aliquid voluptatibus sed tempora cumque nisi non magnam in dolores. Dolor, ex architecto. Corporis ex odio eaque doloremque illum molestias, facere, commodi vitae perspiciatis adipisci quaerat minus ab eveniet praesentium dolorum magnam quibusdam aut iusto.</p>
-           </div>
+            </div>
+
         </section>
     )
 }
