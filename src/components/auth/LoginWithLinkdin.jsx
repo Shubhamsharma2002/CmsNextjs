7997c6ffd9a2735ab2e7014ae7a72aba9fba9206
shubham sharma <shubhamtech004@gmail.com>
2025-08-06 23:19:37 +0530

    testing

--- src/components/auth/LoginWithLinkdin.jsx
+++ src/components/auth/LoginWithLinkdin.jsx
@@ -1,19 +1,18 @@
-
 "use client";
 import { toast, Toaster } from "sonner";
 import { Icons } from "../Icons";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
-
-export  default function  LoginWithLinkdin () {
-  
-  return(
-         <div className="w-full mt-3">
-        <button  className="w-full py-3 px-7 flex items-center justify-center gap-3 rounded-lg shadow-sm  bg-emerald-500 cursor-pointer">
-          <Icons.Linkdin className="w-4 h-8"/>
-          <span className="text-base text-white font-medium">Sign in with Linkdin</span>
-        </button>
-      </div>
-      )
+export default function LoginWithLinkdin() {
+  return (
+    <div className="w-full mt-3">
+      <button className="w-full py-3 px-7 flex items-center justify-center gap-3 rounded-lg shadow-sm  bg-emerald-500 cursor-pointer">
+        <Icons.Linkdin className="w-4 h-8" />
+        <span className="text-base text-white font-medium">
+          Sign in with Linkdin
+        </span>
+      </button>
+    </div>
+  );
 }
