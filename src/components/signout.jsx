6261857c884e6d9ea081e2c5f64d810c9d4502ce
shubham sharma <shubhamtech004@gmail.com>
2025-08-10 20:01:16 +0530

    testing

--- src/components/signout.jsx
+++ src/components/signout.jsx
@@ -1,9 +1,14 @@
-"use client"
+"use client";
 import { LogOut } from "lucide-react";
 import { signOut } from "next-auth/react";
 
-export default function Signout(){
-    return <div onClick={()=> signOut({ callbackUrl: '/sign-in'})} className="flex items-center gap-2"> 
-        <LogOut className="w-4" /> Log out !
+export default function Signout() {
+  return (
+    <div
+      onClick={() => signOut({ callbackUrl: "/sign-in" })}
+      className="flex items-center gap-2"
+    >
+      <LogOut className="w-4" /> Log out !
     </div>
+  );
 }
