c63fd14495facfdb65539de3f7e2fcaa9d2c6def
shubham sharma <shubhamtech004@gmail.com>
2025-08-12 02:20:17 +0530

    admin only acess task done with api also

---
+++ src/app/(admin)/posts/page.jsx
@@ -0,0 +1,21 @@
+import AdminAllPosts from "@/components/admin/adminAllposts";
+import UserAllPosts from "@/components/admin/userAllpost";
+import { authOptions } from "@/lib/authOptions";
+import isAdmin from "@/utils/isAdmin";
+import { getServerSession } from "next-auth";
+
+export default async function AllPosts( {searchParams }){
+    const page = searchParams.page || 1;
+    const category = searchParams.cat || null;
+    const session = await getServerSession(authOptions);
+
+    const adminCheck = await isAdmin(session);
+    console.log(adminCheck, ' admin Check')
+    if(!adminCheck) {
+        return <UserAllPosts page={page} category={category} user={session.user}/>
+      
+    }
+    return <div>
+        <AdminAllPosts page={page} category={category} />
+    </div>
+}
