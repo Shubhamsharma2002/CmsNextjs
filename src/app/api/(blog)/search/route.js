5d048b4f0bea65b65190961b9032d236751d50e2
shubham sharma <shubhamtech004@gmail.com>
2025-08-12 23:42:42 +0530

    search post on blogify api done with the intreface

--- src/app/api/(blog)/search/route.js
+++ src/app/api/(blog)/search/route.js
@@ -1,37 +1,42 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request){
-    const { searchParams } = new URL(request.url);
-    const query = searchParams.get('query');
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get("query");
 
-    if(!query) {
-        return NextResponse.json({
-            message: "Query is blank"
-        }, { status: 404 })
-    }
+  if (!query) {
+    return NextResponse.json(
+      {
+        message: "Query is blank",
+      },
+      { status: 404 }
+    );
+  }
 
-    const posts = await prisma.post.findMany({
-        where: {
-            status: "PUBLISHED",
-            OR: [
-                { title: { contains: query, mode: 'insensitive'}},
-                { content: { contains: query, mode: 'insensitive'}},
-            ]
+  const posts = await prisma.post.findMany({
+    where: {
+      status: "PUBLISHED",
+      OR: [
+        { title: { contains: query, mode: "insensitive" } },
+        { content: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    include: {
+      author: {
+        select: {
+          name: true,
+          image: true,
         },
-        include: {
-            author: {
-                select: {
-                    name: true,
-                    image: true
-                }
-            }
-        }
-    })
-    if(!posts.length) {
-        return NextResponse.json({ message: "No posts with current query found"} ,{ status: 404 })
-    }
+      },
+    },
+  });
+  if (!posts.length) {
+    return NextResponse.json(
+      { message: "No posts with current query found" },
+      { status: 404 }
+    );
+  }
 
-    return NextResponse.json(posts, { status: 200 })
+  return NextResponse.json(posts, { status: 200 });
 }
-
