d04539fb796cc3554ce6643b20ea54c1a39acc0d
shubham sharma <shubhamtech004@gmail.com>
2025-08-12 16:43:03 +0530

    admin see all the user features on this projects

---
+++ src/app/(admin)/users/page.jsx
@@ -0,0 +1,29 @@
+
+
+import AdminAllUsers from "@/components/admin/allUser";
+import { authOptions } from "@/lib/authOptions";
+import isAdmin from "@/utils/isAdmin";
+import { getServerSession } from "next-auth";
+
+export default async function AllUsers(){
+
+
+    const session = await getServerSession(authOptions);
+
+    if(!session){
+        return <section className="w-full h-screen justify-center items-center">
+            You are not Authenticated!
+        </section>
+    }
+
+    const adminCheck = await isAdmin(session);
+
+    if(!adminCheck){
+        return <section className="w-full h-screen justify-center items-center">
+            You are not Authorized
+        </section>
+    }
+
+    return <AdminAllUsers />
+
+}
