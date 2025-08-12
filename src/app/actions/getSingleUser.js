e5b6b16efbc01bbd3d60509719c25f6c83af7519
shubham sharma <shubhamtech004@gmail.com>
2025-08-12 18:04:49 +0530

    get all user on admin dasbord or user details page

---
+++ src/app/actions/getSingleUser.js
@@ -0,0 +1,50 @@
+import { prisma } from "@/lib/prisma";
+
+export  async function getSingleUser(username){
+    const user = await prisma.user.findUnique({
+        where: {
+            username
+        },
+        select: {
+            name: true,
+            image: true, 
+            createdAt: true,
+            username: true,
+            Post: {
+                select: {
+                    title: true,
+                    slug: true,
+                    thumbnail: true,
+                    excerpt: true
+                }
+            }
+        } 
+    })
+    return user;
+}
+
+export  async function getUserById(id) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: id }, // MongoDB ObjectId string
+      select: {
+        name: true,
+        image: true,
+        createdAt: true,
+        username: true,
+        Post: {
+          select: {
+            title: true,
+            slug: true,
+            thumbnail: true,
+            excerpt: true,
+          },
+        },
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return null;
+  }
+}
