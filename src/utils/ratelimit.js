4e5707394f3c6a067af4a1971c75cd1591241ef0
shubham sharma <shubhamtech004@gmail.com>
2025-08-13 23:35:13 +0530

    og image process done

--- src/utils/ratelimit.js
+++ src/utils/ratelimit.js
@@ -2,10 +2,10 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
 export const rateLimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5, '10 s')
-})
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(5, "10 s"),
+});
 
 export const config = {
-    runtime: "edge"
-}
+  runtime: "edge",
+};
