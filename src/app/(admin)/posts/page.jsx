1dd6c50d2fc52fd573b667900a8ccbf90f57c479
shubham sharma <shubhamtech004@gmail.com>
2025-08-25 23:19:11 +0530

    admin page

--- src/app/(admin)/posts/page.jsx
+++ src/app/(admin)/posts/page.jsx
@@ -4,18 +4,19 @@ import { authOptions } from "@/lib/authOptions";
 import isAdmin from "@/utils/isAdmin";
 import { getServerSession } from "next-auth";
 
-export default async function AllPosts( {searchParams }){
-    const page = searchParams.page || 1;
-    const category = searchParams.cat || null;
-    const session = await getServerSession(authOptions);
+export default async function AllPosts({ searchParams }) {
+  const page = searchParams.page || 1;
+  const category = searchParams.cat || null;
+  const session = await getServerSession(authOptions);
 
-    const adminCheck = await isAdmin(session);
-    console.log(adminCheck, ' admin Check')
-    if(!adminCheck) {
-        return <UserAllPosts page={page} category={category} user={session.user}/>
-      
-    }
-    return <div>
-        <AdminAllPosts page={page} category={category} />
+  const adminCheck = await isAdmin(session);
+  console.log(adminCheck, " admin Check");
+  if (!adminCheck) {
+    return <UserAllPosts page={page} category={category} user={session.user} />;
+  }
+  return (
+    <div>
+      <AdminAllPosts page={page} category={category} />
     </div>
+  );
 }
