ccb7fc092c4aaaea4482855824bf9ad09ed38143
shubham sharma <shubhamtech004@gmail.com>
2025-08-28 23:39:26 +0530

    api testing done ...

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import { getAuthsession } from "@/lib/authOptions";
 
 export default async function Dashboard(){
     const session = await getAuthsession();
+    
     if(!session){
         return <section className="flex w-full h-screen justify-center items-center">
         Not Authenticated
